bc0af1d24aa5e97cc755ccbad4e9163276a803c7
ngodinhnhathoang <hoangndn1602@gmail.com>
2019-01-30 20:30:56 +0700

    Add validate for model article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 2, maximum: 500 }
+
 end
