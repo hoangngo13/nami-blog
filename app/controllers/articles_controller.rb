e0e4d0aa8519163afc23ce833243a88f60eb1031
Man Vo <man@Mans-MacBook-Air.local>
2019-02-02 10:51:42 +0700

    Create action edit and update article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update]
+
   def new
     @article = Article.new
   end
@@ -15,12 +17,29 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    @article = Article.find(params[:id])
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article updated success!"
+      redirect_to article_path(@article)
+    else
+
+    end
   end
 
   private
     def article_params
       params.require(:article).permit(:title, :description)
+    end
+
+    def find_article
+      @article = Article.find(params[:id])
     end
 end
   
