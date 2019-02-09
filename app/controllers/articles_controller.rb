ed635e1ae0103bb49c3fd2591c3b1189f848f6c0
Man Vo <man@Mans-MacBook-Air.local>
2019-02-09 20:38:41 +0700

    list articles and layout links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 class ArticlesController < ApplicationController
   before_action :find_article, only: [:show, :edit, :update]
 
+  def index
+    @articles = Article.all
+  end
+  
   def new
     @article = Article.new
   end
