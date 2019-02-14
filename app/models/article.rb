36a87e9dee022e8473069549c45e7541e1ad26d8
ngodinhnhathoang <hoangndn1602@gmail.com>
2019-02-14 21:50:51 +0700

    add user_id to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,6 @@
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: { minimum: 2, maximum: 50 }
   validates :description, presence: true, length: { minimum: 2, maximum: 500 }
-
-  def new
-  end
+  validates :user_id, presence: true
 end
