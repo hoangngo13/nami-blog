ab4ecde7463a92f1b6d2fcc6e76d038f946052ec
Ngo Dinh Nhat Hoang <ngo.dinh.nhat.hoang@framgia.com>
2019-02-17 21:00:14 +0700

    Section 6, Lecture 136

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,20 @@ class SessionsController < ApplicationController
   end
   
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Logged in success"
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "Email or password is not correct. Please try again!"
+      render 'new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have looged out"
+    redirect_to root_path
   end
 end
