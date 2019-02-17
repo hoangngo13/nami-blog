d59607679501244f711ced9574ef164216f92cd5
Man Vo <man@Mans-MacBook-Air.local>
2019-02-17 11:49:20 +0700

    Finish #126

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,42 @@
 class UsersController < ApplicationController
-
+  before_action :find_user, only: [:show, :edit, :update]
+  
+  def index
+    @users = User.all
+  end
   def new
     @user = User.new
   end
 
+  def show
+  end
   def create
     @user = User.new(user_params)
     if @user.save
         flash[:sucess] = "User created success!"
-        redirect_to article_path
-      else
+        redirect_to articles_path
+    else
         render 'new'
-      end
+    end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+        flash[:sucess] = "User updated success"
+        redirect_to articles_path
+    else
+        render edit
+    end
+  end
   private
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+
+    def find_user
+      @user = User.find(params[:id])
     end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_ulr ="https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_ulr, alt: user.username, class:"img-circle")
+  end
 end
